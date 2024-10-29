b97c6a73c44d4abb10458720f249ef356a8679ec
Austin <babelontrip@gmail.com>
2024-10-29 11:17:37 -0600

    git init

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request,Response} from 'express'
+import { createApp } from './config/express';
+
+const app = createApp();
+
+const port = 3000;
+
+// Middleware to handle JSON
+app.use(express.json())
+
+app.get('/', (req:Request, res:Response) => {
+    res.send("hello world!")
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at port http://localhost:${port}`)
+})
