4951a4d0f0ef0adced80c1cf35c278205f814a43
Austin <babelontrip@gmail.com>
2024-10-30 18:33:16 -0600

    second try pipeline

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(express.json())
 
 app.get('/', (req:Request, res:Response) => {
-    res.send("hello world setup pipeline :D!")
+    res.send("hello world setup pipeline :D second try!")
 })
 
 
